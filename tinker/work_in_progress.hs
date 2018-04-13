c92a366d1ccde98dfe54b9f2764f86f7cc58c1fc
Martin <martin.mphil@gmail.com>
2018-04-13 14:35:04 +0100

    my first attempt at defining dice parameters

--- tinker/work_in_progress.hs
+++ tinker/work_in_progress.hs
@@ -1,36 +1,21 @@
 {-
-Test file for learning functional programming;
-pure, immutable and referentially transparent.
+A player rolling equal to or under target number generates a hit
+  as apposed to a miss.
+Party succeeds if any player rolls a hit.
+
+NEXT I want to define
+sided as list
+encounters as an array [force_ratio population sided target]
+
 -}
 
--- define type dice roll meeting or beating taget t
-t :: Int
-t = 17
+-- NEED to define type dice these as functions operating on lists
+force_ratio, population, sided, target :: Double
+force_ratio = 0.5
+population = 3
+sided = 6
+target = 17
 
 -- define type h as hit on a dice roll meeting or beating taget t
-
-
--- clause checked in order, and the first matching clause is chosen.
-
-sumtorial :: Integer -> Integer
-sumtorial 0 = 0
-sumtorial n = n + sumtorial (n-1)
-
---guard tested in order for first match
-hailstone :: Integer -> Integer
-hailstone n
-  | n `mod` 2 == 0 = n `div` 2
-  | otherwise      = 3*n + 1
-
--- function definition
-f :: Int -> Int -> Int -> Int
-f x y z = x + y + z
-
-holeScore :: Int -> Int -> String
-holeScore strokes par
-  | score < 0 = show (abs score) ++ " under par"
-  | score == 0 = "level par"
-  | otherwise = show(score) ++ " over par"
- where score = strokes-par
 
 -- basic unit of dice instructions
