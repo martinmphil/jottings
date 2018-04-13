bcf0d4b316d5835bc8b61c0318b27fbc8390ed89
Martin <martin.mphil@gmail.com>
2018-04-13 09:48:45 +0100

    small changes to comments

--- elementary_program.hs
+++ elementary_program.hs
@@ -3,6 +3,11 @@ pure, immutable and
 referentially transparent
 -}
 
--- main function
+-- define type dice roll meeting or beating taget t
+t :: Int
+t = 17
 
+-- define type h as hit on a dice roll meeting or beating taget t
+
+-- printing dice instructions
 main = putStrLn "Roll 3d20 target 17"
