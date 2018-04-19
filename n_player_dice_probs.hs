270fe16f1aae5a5805753a829e94bc5931a5c6f7
Martin <martin.mphil@gmail.com>
2018-04-19 08:45:47 +0100

    Haskell prodution code for polyhedral dice probabilities for upto 9 players

--- n_player_dice_probs.hs
+++ n_player_dice_probs.hs
@@ -1,4 +1,4 @@
-{- martinmphil@gmail.com
+{- Martin.MPhil@gmail.com
 Theoretical force_ratio is analogous to an Us-vs-Them ratio created by specific user scenarios.
 Assuming: (i) the whole party succeeds if any player meets or beats a target number, and
 (ii) every participating player rolls one dice each and these rolled dice all have equal number of sides,
@@ -34,6 +34,42 @@ thus p(miss) = ((t-1)/s)
 and with "n" players all rolling the same type of polyhedral dice,
 p(all_miss) = (((t-1)/s)**n)
 therefore p(success) = 1 - (((t-1)/s)**n)
+
+In order to mitigate decimal rounding errors (potentially ^9),
+force-ratios are multiplied by *1e6.
+
+"vs" returns a force-ratio for "n" players
+rolling dice with "s" sides
+attempting to meet or beat a target "t".
+
+"e" presents thirty one possible polyhedral dice combinations.
+
+"r" multiplexes for one to nine players
+and applies function "vs"
+
+"d" presents data for output
+in the order of one through to nine players.
 -}
 
-
+vs :: [Double] -> Double
+vs x | p <= 0.5 = (2*p)*1e6
+     | p > 0.75 = (1/(2-2*p))*1e6
+     | otherwise = ((4*p)-1)*1e6
+       where n = head x
+             s = head (tail x)
+             t = last x
+             p = (1 - (((t-1)/s)**n))
+e :: [[Double]]
+e = [[6,2],[6,3],[6,4],[6,5],[6,6],
+     [8,2],[8,3],[8,4],[8,6],[8,7],[8,8],[12,2],[12,6],[12,8],[12,12],
+     [20,2],[20,3],[20,4],[20,5],[20,7],[20,8],[20,9],[20,10],
+     [20,12],[20,13],[20,14],[20,15],[20,17],[20,18],[20,19],[20,20]]
+r :: Double -> [Double]
+r x = map (vs) (map (x:) e)
+a :: Double -> t -> [[Double]]
+a m z | m <= 0  = []
+      | otherwise = r m : a (m-1) z
+d :: [[Double]]
+d = reverse (a 9 r)
+main :: IO ()
+main = putStr "full_dice_force_ratio_arr = " >> print d
