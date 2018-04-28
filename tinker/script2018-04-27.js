43f947dcd57fe1dad7a7647449f6c11f5ebef1a4
Martin <martin.mphil@gmail.com>
2018-04-28 09:34:34 +0100

    using (1/a)*a===1 to test for identical fractions with differing numerators and denominators

--- tinker/script2018-04-27.js
+++ tinker/script2018-04-27.js
@@ -1,6 +1,19 @@
-var double = x => x * 2;
+"use strict";
 
-var a = R.map(double, [1, 2, 3]);
+let numer000 = 1;
+let denom000 = 2;
+
+let numer001 = 8;
+let denom001 = 16;
+
+function fractionalTest (j,k,l,m) {
+  let p = j/k;
+  let q = l/m;
+  return p * (1/q);
+}
+
+
+var a = fractionalTest(numer000, denom000, numer001, denom001);
 
 console.log(a);
 
