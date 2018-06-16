6d1f08272ce7815812c3f9059029a133459a81f3
Martin <martin.mphil@gmail.com>
2018-06-16 01:22:14 +0100

    dividing click functions and updating enclosure

--- tinker/script2018-04-28.js
+++ tinker/script2018-04-28.js
@@ -28,8 +28,8 @@ function pos_finite_predicate(x) {
 }
 
 // [1]
-var e = (function () {
-  var max_player_nbrs = 9;
+const e = (function () {
+  var max_player_nbrs = 6;
   var player_nbrs = 3;
   var us = 10;
   var them = 10;
@@ -37,17 +37,18 @@ var e = (function () {
   var adv_them = 0;
   var dice_available = [6, 8, 12, 20, 100]; // 6, 8, 12, 20, 100  n9 110 values
   var enc = {};
-  enc.change_n = x => Number.isInteger(x) && x > 0 && x < max_player_nbrs ?
+  enc.reset = {n:player_nbrs, u:us, t:them, ua:adv_us, ta:adv_them};
+  enc.change_n = x => Number.isInteger(x) && x > 0 && x <= max_player_nbrs ?
     player_nbrs = x : player_nbrs = player_nbrs;
   enc.change_us = x => pos_finite_predicate(x) ? us = x : us = us;
   enc.change_them = x => pos_finite_predicate(x) ? them = x : them = them;
-  enc.change_adv_us = x => Number.isFinite(x) ? adv_us = x : adv_us = adv_us;
-  enc.change_adv_them = x => Number.isFinite(x) ? adv_them = x : adv_them = adv_them;
+  enc.change_us_adv = x => Number.isFinite(x) ? adv_us = x : adv_us = adv_us;
+  enc.change_them_adv = x => Number.isFinite(x) ? adv_them = x : adv_them = adv_them;
   enc.get_n = () => player_nbrs;
   enc.get_us = () => us;
   enc.get_them = () => them;
-  enc.get_adv_us = () => adv_us;
-  enc.get_adv_them = () => adv_them;
+  enc.get_us_adv = () => adv_us;
+  enc.get_them_adv = () => adv_them;
   // [2]
   enc.force_ratio_recur = function chances(x = max_player_nbrs) {
     if (x < 1) return;
@@ -79,18 +80,34 @@ function instructions(n, us_vs_them) {
   let fr_array = e['a' + n].map( ([x,y]) => y );
   let difference_array = fr_array.map( x => Math.abs(x - us_vs_them) );
   let least_difference = difference_array.reduce( (x, y) => Math.min(x, y) );
-  // index of least difference
+  // index of least different
   let i = difference_array.indexOf(least_difference);
   return e['a' + n][i][0];
 }
 
 function instruct() {
   let us_vs_them = calculate_us_vs_them(
-    e.get_us(), e.get_them(), e.get_adv_us(), e.get_adv_them()
+    e.get_us(), e.get_them(), e.get_us_adv(), e.get_them_adv()
   );
   let ndst = instructions(e.get_n(), us_vs_them);
   document.querySelector('#dice_roll_instructions').textContent =
     `Roll ${ndst}.`;
+}
+
+function reset_button(){
+  e.change_n(e.reset.n);
+  e.change_us(e.reset.u);
+  e.change_them(e.reset.t);
+  e.change_us_adv(e.reset.ua);
+  e.change_them_adv(e.reset.ta);
+  document.querySelector('.pickedBttn').classList.add('availBttn');
+  document.querySelector('.pickedBttn').classList.remove('pickedBttn');
+  prepare_player_numbers_section();
+  document.querySelector('#us_input').value = e.reset.u;
+  document.querySelector('#them_input').value = e.reset.t;
+  document.querySelector('#us_adv_input').value = e.reset.ua;
+  document.querySelector('#them_adv_input').value = e.reset.ta;
+  instruct();
 }
 
 function prepare_player_numbers_section() {
@@ -99,6 +116,7 @@ function prepare_player_numbers_section() {
   document.querySelectorAll('#pNbrs > input[type=button]').forEach( i => {
     i.addEventListener('click', () => {
       e.change_n(parseInt(i.value));
+      instruct();
       document.querySelector(".pickedBttn").classList.add("availBttn");
       document.querySelector(".pickedBttn").classList.remove("pickedBttn");
       i.classList.remove('availBttn');
@@ -108,35 +126,90 @@ function prepare_player_numbers_section() {
 }
 
 function prepare_keypads() {
-  document.querySelectorAll('.nbr_keypad').forEach( i => {
+  document.querySelectorAll('.us_nbr_keypad').forEach( i => {
     i.addEventListener('click', () => {
-        keypad_button_fn(i.value);
+        us_keypad_button_fn(i.value);
       })
   })
 }
 
-function keypad_button_fn(x) {
+function us_keypad_button_fn(x) {
+  let el = document.querySelector('#us_input');
+  let result_string = el.value + x;
+  el.value = result_string;
+  e.change_us(Number.parseFloat(result_string));
+  instruct();
+  //remove
   console.log('keypad_button_fn ' + x);
+  console.log('el ' + el.value);
+  console.log(Number.parseFloat(result_string));
 }
 
+function us_clear_button() {
+  let el = document.querySelector('#us_input');
+  let result_value = Number.parseFloat(el.value) * 2;
+  e.change_us(result_value);
+  el.value = result_value;
+  console.log('inside clear button' + el.value);
+  instruct();
+}
 // remove
 //e.change_n(4);
-e.change_us(33);
-e.change_them(22);
-e.change_adv_us(3);
-e.change_adv_them(2);
+//e.change_us(33);
+//e.change_them(22);
+//e.change_us_adv(2);
+//e.change_them_adv(2);
 //var theState = e.get_state();
 //console.log(theState);
 
-
+function us_input() {
+  let el = document.querySelector('#us_input');
+  e.change_us(Number.parseFloat(el.value));
+  el.addEventListener('input', () => {
+    e.change_us(Number.parseFloat(el.value));
+    instruct();
+  });
+}
+function them_input() {
+  let el = document.querySelector('#them_input');
+  e.change_them(Number.parseFloat(el.value));
+  el.addEventListener('input', () => {
+    e.change_them(Number.parseFloat(el.value));
+    instruct();
+  });
+}
+function us_adv_input() {
+  let el = document.querySelector('#us_adv_input');
+  e.change_us_adv(Number.parseFloat(el.value));
+  el.addEventListener('input', () => {
+    e.change_us_adv(Number.parseFloat(el.value));
+    instruct();
+  });
+}
+function them_adv_input() {
+  let el = document.querySelector('#them_adv_input');
+  e.change_them_adv(Number.parseFloat(el.value));
+  el.addEventListener('input', () => {
+    e.change_them_adv(Number.parseFloat(el.value));
+    instruct();
+  });
+}
 
 function main() {
   e.force_ratio_recur();
   prepare_player_numbers_section();
-  prepare_keypads()
+  prepare_keypads();
+  us_input();
+  them_input();
+  us_adv_input();
+  them_adv_input();
   instruct();
+  document.querySelector('#reset_button').addEventListener( 'click', reset_button );
+  document.querySelector('#us_clear_button').addEventListener( 'click', us_clear_button )
 }
 main();
 
 // remove
 console.log('performance '+ performance.now());
+var theState = e.get_state();
+console.log(theState);
