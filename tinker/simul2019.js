3600e7ad4041b13883dbb0757107c42e1bfd3454
Martin <martin.mphil@gmail.com>
2018-06-18 08:15:56 +0100

    final functionality and linter improvements

--- tinker/simul2019.js
+++ tinker/simul2019.js
@@ -1,84 +1,82 @@
 'use strict';
+// Pure functions with "n" players, "s" sided dice and target roll "t".
 function force_ratio(n, s, t) {
-  let p = (1 - (((t-1)/s)**n));
-  let result;
-  p <= 0.5 ? result = (2 * p)
-    : p > 0.75 ? result = (1 / (2 - 2 * p)) : result = ((4 * p) - 1);
+  let p = ( 1 - Math.pow( (( t- 1) / s), n) );
+  //let p = (1 - (((t-1)/s)**n));
+  let result = (p <= 0.5) ? (2 * p)
+    : (p > 0.75) ? (1 / (2 - 2 * p)) : ((4 * p) - 1);
   return result;
 }
 function range_t(s) {
   let result = [];
   (function range_recur(x) {
-    if (x <= 1) return result;
+    if (x <= 1) {return result;}
     result[x - 2] = x;
     return range_recur(x - 1);
   })(s);
   return result.reverse();
 }
 function dice_s_t_pairs(x) {
-  return range_t(x).map( y => [x, y] );
+  return range_t(x).map( (y) => [x, y] );
 }
 function s_t_longlist(x) {
-  return x.map( y => dice_s_t_pairs(y) ).reduce((a, b) => a.concat(b));
+  return x.map( (y) => dice_s_t_pairs(y) ).reduce((a, b) => a.concat(b));
 }
 function pos_finite_predicate(x) {
   if (x > 0 && Number.isFinite(x)) {
     return true;
   }
 }
-
-// [1]
+/*
+Impure functions below including:
+state closure "e" with: protagonist total, "us"; antagonist total, "them";
+advantage us, "adv_us"; advantage them, "adv_them";
+array of polyhedral dice sides, "dice_available".
+*/
 const e = (function () {
-  var max_player_nbrs = 6;
-  var player_nbrs = 3;
-  var us = 10;
-  var them = 10;
-  var adv_us = 0;
-  var adv_them = 0;
-  var dice_available = [6, 8, 12, 20, 100]; // 6, 8, 12, 20, 100  n9 110 values
-  var enc = {};
+  let max_player_nbrs = 6;
+  let player_nbrs = 3;
+  let us = 10;
+  let them = 10;
+  let adv_us = 0;
+  let adv_them = 0;
+  let dice_available = [6, 8, 12, 20, 100]; // [6, 8, 12, 20, 100]
+  let enc = {};
   enc.reset = {n:player_nbrs, u:us, t:them, ua:adv_us, ta:adv_them};
-  enc.change_n = x => Number.isInteger(x) && x > 0 && x <= max_player_nbrs ?
-    player_nbrs = x : player_nbrs = player_nbrs;
-  enc.change_us = x => pos_finite_predicate(x) ? us = x : us = us;
-  enc.change_them = x => pos_finite_predicate(x) ? them = x : them = them;
-  enc.change_us_adv = x => Number.isFinite(x) ? adv_us = x : adv_us = adv_us;
-  enc.change_them_adv = x => Number.isFinite(x) ? adv_them = x : adv_them = adv_them;
+  enc.change_n = (x) => player_nbrs =
+    (Number.isInteger(x) && x > 0 && x <= max_player_nbrs) ? x : player_nbrs;
+  enc.change_us = (x) => us = pos_finite_predicate(x) ? x : us;
+  enc.change_them = (x) => them = pos_finite_predicate(x) ? x : them;
+  enc.change_us_adv = (x) => adv_us = Number.isFinite(x) ? x : adv_us;
+  enc.change_them_adv = (x) => adv_them = Number.isFinite(x) ? x : adv_them;
   enc.get_n = () => player_nbrs;
   enc.get_us = () => us;
   enc.get_them = () => them;
   enc.get_us_adv = () => adv_us;
   enc.get_them_adv = () => adv_them;
-  // [2]
+  // fn returning lists of [instruction, force_ratio] in nested arrays for each n
   enc.force_ratio_recur = function chances(x = max_player_nbrs) {
-    if (x < 1) return;
+    if (x < 1) {return;}
     let result = s_t_longlist(dice_available)
       .map ( ([y, z]) => [`${x}d${y} target ${z}`, force_ratio(x, y, z)] );
-    enc['a' + x] = result.filter( ([j, k]) => pos_finite_predicate(k) );
+    enc['a' + x] = result.filter( ([, k]) => pos_finite_predicate(k) );
     return chances(x - 1);
   };
-
-//remove
-  enc.get_state = () => [player_nbrs, us, them, adv_us,adv_them];
-
   return enc;
 }());
-
 function calculate_us_vs_them(us, them, adv_us, adv_them) {
-  let force_multiplier = 1;
   let delta_adv = adv_us - adv_them;
   function logarithmic_scaling(delta){
-    return 2 * Math.log(Math.abs(delta) + 1)
+    return 2 * Math.log(Math.abs(delta) + 1);
   }
-  delta_adv > 0 ? force_multiplier = logarithmic_scaling(delta_adv) :
-    delta_adv < 0 ? force_multiplier = ( 1 / logarithmic_scaling(delta_adv) ) :
-      force_multiplier = force_multiplier;
+  let force_multiplier =
+  (delta_adv > 0) ? logarithmic_scaling(delta_adv) :
+    (delta_adv < 0) ? ( 1 / logarithmic_scaling(delta_adv) ) : 1;
   return (us / them) * force_multiplier;
 }
-
 function instructions(n, us_vs_them) {
-  let fr_array = e['a' + n].map( ([x,y]) => y );
-  let difference_array = fr_array.map( x => Math.abs(x - us_vs_them) );
+  let fr_array = e['a' + n].map( ([,y]) => y );
+  let difference_array = fr_array.map( (x) => Math.abs(x - us_vs_them) );
   let least_difference = difference_array.reduce( (x, y) => Math.min(x, y) );
   // index of least different
   let i = difference_array.indexOf(least_difference);
@@ -92,7 +90,6 @@ function instruct() {
   document.querySelector('#dice_roll_instructions').textContent =
     `Roll ${ndst}.`;
 }
-
 function reset_button(){
   e.change_n(e.reset.n);
   e.change_us(e.reset.u);
@@ -162,15 +159,40 @@ function them_clear_button() {
   el.value = result_string;
   instruct();
 }
-// remove
-//e.change_n(4);
-//e.change_us(33);
-//e.change_them(22);
-//e.change_us_adv(2);
-//e.change_them_adv(2);
-//var theState = e.get_state();
-//console.log(theState);
-
+function prepare_adv_buttons() {
+  document.querySelector('#us_adv_minus_button')
+    .addEventListener('click', () =>
+      {let result = e.get_us_adv() - 1;
+      e.change_us_adv(result);
+      document.querySelector('#us_adv_input').value = result;
+      instruct();
+      }
+    );
+  document.querySelector('#us_adv_plus_button')
+    .addEventListener('click', () =>
+      {let result = e.get_us_adv() + 1;
+      e.change_us_adv(result);
+      document.querySelector('#us_adv_input').value = result;
+      instruct();
+      }
+    );
+  document.querySelector('#them_adv_minus_button')
+    .addEventListener('click', () =>
+      {let result = e.get_them_adv() - 1;
+      e.change_them_adv(result);
+      document.querySelector('#them_adv_input').value = result;
+      instruct();
+      }
+    );
+  document.querySelector('#them_adv_plus_button')
+    .addEventListener('click', () =>
+      {let result = e.get_them_adv() + 1;
+      e.change_them_adv(result);
+      document.querySelector('#them_adv_input').value = result;
+      instruct();
+      }
+    );
+}
 function us_input() {
   let el = document.querySelector('#us_input');
   e.change_us(Number.parseFloat(el.value));
@@ -203,23 +225,21 @@ function them_adv_input() {
     instruct();
   });
 }
-
 function main() {
   e.force_ratio_recur();
   prepare_player_numbers_section();
   prepare_keypads();
+  prepare_adv_buttons();
   us_input();
   them_input();
   us_adv_input();
   them_adv_input();
-  document.querySelector('#reset_button').addEventListener( 'click', reset_button );
-  document.querySelector('#us_clear_button').addEventListener( 'click', us_clear_button )
-  document.querySelector('#them_clear_button').addEventListener( 'click', them_clear_button )
+  document.querySelector('#reset_button')
+    .addEventListener( 'click', reset_button );
+  document.querySelector('#us_clear_button')
+    .addEventListener( 'click', us_clear_button );
+  document.querySelector('#them_clear_button')
+    .addEventListener( 'click', them_clear_button );
   instruct();
 }
 main();
-
-// remove
-console.log('performance '+ performance.now());
-var theState = e.get_state();
-console.log(theState);
