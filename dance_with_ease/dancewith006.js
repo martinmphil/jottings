24cb71121cbf5ccdcdb10ce52bb7c0a74b9e2be7
Martin <martin.mphil@gmail.com>
2019-03-16 08:32:32 +0000

    vue nav drawer

--- dance_with_ease/dancewith006.js
+++ dance_with_ease/dancewith006.js
@@ -1,16 +1,24 @@
-'use strict';
+// 'use strict';
 
-function main() {
-  var o = document.querySelector('#navigation-drawer-toggle-bttn');
-  var c = document.querySelector('#close-navigation-drawer-bttn');
-  var m = document.querySelector('#navigation-drawer');
-  o.addEventListener('click', function () {
-    m.classList.toggle('navigation-drawer-hidden');
-    o.style.display = "none";
-  });
-  c.addEventListener('click', function () {
-    m.classList.toggle('navigation-drawer-hidden');
-    o.style.display = "block"
-  });
-};
-main();
+// function main() {
+//   var o = document.querySelector('#navigation-drawer-toggle-bttn');
+//   var c = document.querySelector('#close-navigation-drawer-bttn');
+//   var m = document.querySelector('#navigation-drawer');
+//   o.addEventListener('click', function () {
+//     m.classList.toggle('navigation-drawer-hidden');
+//     o.style.display = "none";
+//   });
+//   c.addEventListener('click', function () {
+//     m.classList.toggle('navigation-drawer-hidden');
+//     o.style.display = "block"
+//   });
+// };
+// main();
+
+
+var vm = new Vue({
+  el: '#navigation-drawer',
+  data: {
+    showNavigationDrawer: false
+  }
+})
