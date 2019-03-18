8e07be27e861dcf3d1868a105ab874385f998243
Martin <37618836+martinmphil@users.noreply.github.com>
2019-03-18 10:15:19 +0000

    removed redundent comments

--- dance_with_ease/dancewith006.js
+++ dance_with_ease/dancewith006.js
@@ -1,21 +1,3 @@
-// 'use strict';
-
-// function main() {
-//   var o = document.querySelector('#navigation-drawer-toggle-bttn');
-//   var c = document.querySelector('#close-navigation-drawer-bttn');
-//   var m = document.querySelector('#navigation-drawer');
-//   o.addEventListener('click', function () {
-//     m.classList.toggle('navigation-drawer-hidden');
-//     o.style.display = "none";
-//   });
-//   c.addEventListener('click', function () {
-//     m.classList.toggle('navigation-drawer-hidden');
-//     o.style.display = "block"
-//   });
-// };
-// main();
-
-
 var vm = new Vue({
   el: '#navigation-drawer',
   data: {
