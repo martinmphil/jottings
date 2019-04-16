dce7012d3c8fcc1ddaf5a3adb92c3ab724ff1685
Martin <martin.mphil@gmail.com>
2019-04-16 09:48:09 +0100

    updated React-from-hooks

--- react_from_hooks/fromhooks.js
+++ react_from_hooks/fromhooks.js
@@ -2,9 +2,12 @@ function main () {
   let a = `
     <li><a href="./intro_to_hooks.html">Intro</a></li>
     <li><a href="./jsx.html">JSX</a></li>
-    <li><a href="./state_hook.html">State Hook</a></li>
-    <li><a href="./effects_hook.html">Effects Hook</a></li>
-    <li><a href="./custom_hook.html">Custom Hook</a></li>
+    <li><a href="./component.html">Component</a></li>
+    <li><a href="./props.html">Props</a></li>
+    <li><a href="./function_component.html">Function component</a></li>
+    <li><a href="./state_hook.html">State hook</a></li>
+    <li><a href="./effects_hook.html">Effects hook</a></li>
+    <li><a href="./custom_hook.html">Custom hook</a></li>
 
     <li><a href="./sources.html">Sources</a></li>
   `
