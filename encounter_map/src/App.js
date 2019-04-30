2877b142701f4fd355d6840ebd5e6b4399a52f47
Martin <martin.mphil@gmail.com>
2019-04-30 16:25:00 +0100

    rigid turn order

--- encounter_map/src/App.js
+++ encounter_map/src/App.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
 import redSvg from './assets/red_dot.svg'
+import orangeSvg from './assets/orange_dot.svg'
+import yellowSvg from './assets/yellow_dot.svg'
 import blueSvg from './assets/blue_dot.svg'
+import purpleSvg from './assets/purple_dot.svg'
 
 function App() {
 
   let [turn, setTurn] = useState('red')
 
-  function handleDotActive(event) {
-    setTurn(event.target.id)
+  let turnOrder = ['red', 'orange', 'yellow', 'blue', 'purple']
+
+  function advanceTurnOrder(hue) {
+    let i = turnOrder.indexOf(hue)
+    if ( (i + 1) === turnOrder.length) {
+      setTurn(turnOrder[0])
+    } else {
+      setTurn(turnOrder[i + 1])
+    }
   }
 
   function moveDot(hue, x, y) {
@@ -17,6 +27,7 @@ function App() {
     dot.style.position = 'absolute'
     dot.style.left = (x - 20) + 'px'
     dot.style.top = (y - 20) + 'px'
+    advanceTurnOrder(hue)
   }
 
   function handleDotMove(event) {
@@ -24,16 +35,27 @@ function App() {
       moveDot('red', event.clientX, event.clientY)
       // setTurn('blue')
     }
+    if (turn === 'orange') {
+      moveDot('orange', event.clientX, event.clientY)
+    }
+    if (turn === 'yellow') {
+      moveDot('yellow', event.clientX, event.clientY)
+    }
     if (turn === 'blue') {
       moveDot('blue', event.clientX, event.clientY)
-      // setTurn('red')
+    }
+    if (turn === 'purple') {
+      moveDot('purple', event.clientX, event.clientY)
     }
   }
 
   return (
     <div className="App" onClick={handleDotMove}>
-      <img src={redSvg} id="redDot" alt="red dot" onClick={handleDotActive}/>
-      <img src={blueSvg} id="blueDot" alt="blue dot" onClick={handleDotActive}/>
+      <img src={redSvg} id="redDot" className="dot" alt="red dot" />
+      <img src={orangeSvg} id="orangeDot" className="dot" alt="blue dot" />
+      <img src={yellowSvg} id="yellowDot" className="dot" alt="blue dot" />
+      <img src={blueSvg} id="blueDot" className="dot" alt="blue dot" />
+      <img src={purpleSvg} id="purpleDot" className="dot" alt="blue dot" />
     </div>
   );
 }
