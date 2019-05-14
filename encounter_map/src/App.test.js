84b3cd31ab3f93d709bdac9e7154e173ee2e2e46
Martin <martin.mphil@gmail.com>
2019-05-14 10:22:56 +0100

    improved testing script

--- encounter_map/src/App.test.js
+++ encounter_map/src/App.test.js
@@ -1,5 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
 import App from './App'
 
-it('not to be null', () => {
-  expect(App).not.toBeNull()
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  document.body.removeChild(container)
+  container = null
+})
+
+it('renders redDot svg image', () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  })
+  const rd = container.querySelector('#redDot')
+  expect(rd).toHaveProperty('src')
+  expect(rd.src).toMatch(/.svg/)
 })
