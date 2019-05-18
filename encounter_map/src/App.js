21e173337593ad0d808955ac14e25310d032d438
Martin <martin.mphil@gmail.com>
2019-05-18 08:14:50 +0100

    detect collision for red agent

--- encounter_map/src/App.js
+++ encounter_map/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { ReactComponent as CastleMap } from './assets/castle_map.svg'
+import { ReactComponent as CastleMap } from './assets/castle_map.svg';
 
 function App() {
 
@@ -10,9 +10,13 @@ function App() {
 
   useEffect( () => {
     if (agency !== ''){
-      document.getElementById(agency).classList.add('active')
+      const el = document.getElementById(agency)
+      function afterAnimation() {el.classList.remove('active')}
+      el.classList.add('active')
+      el.addEventListener( "animationend", afterAnimation )
       return () => {
-        document.getElementById(agency).classList.remove('active')
+        el.classList.remove('active')
+        el.removeEventListener( "animationend", afterAnimation )
       }
     }
   })
@@ -32,8 +36,23 @@ function App() {
     }
   }
 
+  function openGround (x,y) {
+      const agentList = document.getElementById('redAgent')
+      const agentAreas = agentList.getBoundingClientRect()
+      let x1 = agentAreas.left + 2
+      let x2 = x1 + agentAreas.width -2
+      let y1 = agentAreas.top +2 
+      let y2 = y1 + agentAreas.height -2
+
+      if (x > x1 && x < x2 && y > y1 && y < y2 ) {
+        alert('Collission')
+        return false
+      }
+      else return true
+  }
+
   function movingAgent(e) {
-    if (turn === 'movingAgent') {
+    if (turn === 'movingAgent' && openGround(e.clientX,e.clientY)) {
       let agent = document.getElementById(agency)
       agent.setAttributeNS(null,"transform",`translate(${e.clientX},${e.clientY})`)
       setTurn('selectingAgent')
@@ -68,9 +87,6 @@ function App() {
         <circle cx="0" cy="0" r="25" />
         <circle cx="10" cy="10" r="25" />
       </g>
-
-
-
 
     </svg>
   )
