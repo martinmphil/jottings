45365a7e7a7392a1ccb2b0931471bf6ee33962cc
Martin <martin.mphil@gmail.com>
2020-01-25 10:29:21 +0000

    bespoke answer grid style

--- judgement_tests/src/App.tsx
+++ judgement_tests/src/App.tsx
@@ -7,7 +7,14 @@ import Q1 from "./q1";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header>Link home header</header>
+      <header>Link home in header</header>
+      <h1>Instructions</h1>
+      <p>
+        Please use one of the left hand buttons to pick your "best" (most
+        effective) response, AND pick your "worst" (least effective) response by
+        selecting one of the right hand buttons.
+      </p>
+      <p>After selecting your answers, please press the "submit" button.</p>
 
       <Q1 />
 
--- judgement_tests/src/q1.tsx
+++ judgement_tests/src/q1.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-const submitHandling = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
+const submitHandling = () => {
   alert("you just pressed the submit button");
 };
 
@@ -12,70 +11,112 @@ const Q1: React.FC = () => {
 
       <h1>Question one</h1>
       <p>
-        You have recently taken a call from a customer, praising the helpful
+        You have recently taken a call from a customer praising the helpful
         service offered by a colleague in your team. Do you:
       </p>
 
       <hr />
-      <button type="button">Best</button>
-      <span>
-        | Tell your colleague about the feedback on a one-to-one basis |
-      </span>
-      <button type="button">Worst</button>
+
+      <div id="answerOption1" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt1"
+          className="answer-button best-button"
+          aria-label="Option 1 is best"
+        >
+          Best
+        </button>
+        <span id="optText1" className="option-text" aria-label="Option 1">
+          Tell your colleague about the feedback on a one-to-one basis
+        </span>
+        <button
+          type="button"
+          id="worstOpt1"
+          className="answer-button worst-button"
+          aria-label="Option 1 is worst"
+        >
+          Worst
+        </button>
+      </div>
+
       <hr />
 
-      <form className="answerForm" onSubmit={submitHandling}>
-        <fieldset>
-          <legend>Q1 answer choices</legend>
+      <div id="answerOption2" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt2"
+          className="answer-button best-button"
+          aria-label="Option 2 is best"
+        >
+          Best
+        </button>
+        <span id="optText2" className="option-text" aria-label="Option 2">
+          Inform your team leader and let them give the praise personally
+        </span>
+        <button
+          type="button"
+          id="worstOpt2"
+          className="answer-button worst-button"
+          aria-label="Option 2 is worst"
+        >
+          Worst
+        </button>
+      </div>
 
-          <hr></hr>
+      <hr />
 
-          <ul>
-            <li>
-              <input type="radio" id="alpha" name="Q1Answer" value="2" />
-              <label htmlFor="alpha">
-                1. Tell your colleague about the feedback on a one-to-one basis
-              </label>
-            </li>
+      <div id="answerOption3" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt3"
+          className="answer-button best-button"
+          aria-label="Option 3 is best"
+        >
+          Best
+        </button>
+        <span id="optText3" className="option-text" aria-label="Option 3">
+          Wait until the team meeting tomorrow and announce the feedback to
+          everyone
+        </span>
+        <button
+          type="button"
+          id="worstOpt3"
+          className="answer-button worst-button"
+          aria-label="Option 3 is worst"
+        >
+          Worst
+        </button>
+      </div>
 
-            <hr></hr>
+      <hr />
 
-            <li>
-              <input type="radio" id="beta" name="Q1Answer" value="3" />
-              <label htmlFor="beta">
-                2. Inform your team leader and let them give the praise
-                personally
-              </label>
-            </li>
+      <div id="answerOption4" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt4"
+          className="answer-button best-button"
+          aria-label="Option 4 is best"
+        >
+          Best
+        </button>
+        <span id="optText4" className="option-text" aria-label="Option 4">
+          Email the whole company so that everyone hears about the great
+          feedback
+        </span>
+        <button
+          type="button"
+          id="worstOpt4"
+          className="answer-button worst-button"
+          aria-label="Option 4 is worst"
+        >
+          Worst
+        </button>
+      </div>
+      <hr />
 
-            <hr></hr>
-
-            <li>
-              <input type="radio" id="gamma" name="Q1Answer" value="3" />
-              <label htmlFor="gamma">
-                3. Wait until the team meeting tomorrow and announce the
-                feedback to everyone
-              </label>
-            </li>
-
-            <hr></hr>
-
-            <li>
-              <input type="radio" id="delta" name="Q1Answer" value="3" />
-              <label htmlFor="delta">
-                4. Email the whole company so that everyone hears about the
-                great feedback
-              </label>
-            </li>
-          </ul>
-
-          <hr></hr>
-        </fieldset>
-
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <button type="submit" onClick={submitHandling}>
+        Submit
+      </button>
     </section>
   );
 };
