d04e9aaa6b1cf8a165f3c413ad82c18a9b90dd7a
Martin <martin.mphil@gmail.com>
2020-01-30 17:58:29 +0000

    abstracted question component

--- judgement_tests/src/App.tsx
+++ judgement_tests/src/App.tsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Q1 from "./q1";
+import Question from "./Question";
+import SampleQuestionText from "./SampleQuestionText";
+
 // const logo = require("./logo.svg") as string;
-// import logo from './logo.svg';
 
 const App: React.FC = () => {
+  const [q1B, setQ1B] = useState(0);
+
+  const bestOpt1 = () => {
+    setQ1B(1);
+  };
+
+  const submitHandling = () => {
+    alert("you just pressed the submit button");
+  };
+
   return (
     <div className="App">
       <header>Link home in header</header>
       <h1>Instructions</h1>
       <p>
-        Please use one of the left hand buttons to pick your "best" (most
-        effective) response, AND pick your "worst" (least effective) response by
-        selecting one of the right hand buttons.
+        Please select one of the buttons on the left to pick your "best" (most
+        effective) response, AND select one of the buttons on the right to pick
+        your "worst" (least effective) response.
       </p>
-      <p>After selecting your answers, please press the "submit" button.</p>
+      <p>After selecting your answers, press the "submit" button.</p>
 
-      <Q1 />
+      <p>TO REMOVE</p>
+      <p>q1B is: {q1B}</p>
 
-      <hr></hr>
+      <Question
+        scenarioText={SampleQuestionText.scenarioText}
+        optText1={SampleQuestionText.optText1}
+        optText2={SampleQuestionText.optText2}
+        optText3={SampleQuestionText.optText3}
+        optText4={SampleQuestionText.optText4}
+        submitHandling={submitHandling}
+        bestOpt1={bestOpt1}
+      />
+
       <nav>Link home nav</nav>
     </div>
   );
---
+++ judgement_tests/src/Question.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+interface Props {
+  scenarioText: string;
+  optText1: string;
+  optText2: string;
+  optText3: string;
+  optText4: string;
+  submitHandling: () => void;
+  bestOpt1: () => void;
+}
+
+const Question: React.FC<Props> = props => {
+  return (
+    <section>
+      <hr />
+
+      <h1>Question one</h1>
+      <p id="scenarioText">{props.scenarioText}</p>
+
+      <hr />
+
+      <div id="answerOption1" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt1"
+          className="answer-button best-button"
+          aria-label="Option 1 is best"
+          onClick={props.bestOpt1}
+        >
+          Best
+        </button>
+        <span id="optText1" className="option-text" aria-label="Option 1">
+          {props.optText1}
+        </span>
+        <button
+          type="button"
+          id="worstOpt1"
+          className="answer-button worst-button"
+          aria-label="Option 1 is worst"
+        >
+          Worst
+        </button>
+      </div>
+
+      <hr />
+
+      <div id="answerOption2" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt2"
+          className="answer-button best-button"
+          aria-label="Option 2 is best"
+        >
+          Best
+        </button>
+        <span id="optText2" className="option-text" aria-label="Option 2">
+          {props.optText2}
+        </span>
+        <button
+          type="button"
+          id="worstOpt2"
+          className="answer-button worst-button"
+          aria-label="Option 2 is worst"
+        >
+          Worst
+        </button>
+      </div>
+
+      <hr />
+
+      <div id="answerOption3" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt3"
+          className="answer-button best-button"
+          aria-label="Option 3 is best"
+        >
+          Best
+        </button>
+        <span id="optText3" className="option-text" aria-label="Option 3">
+          {props.optText3}
+        </span>
+        <button
+          type="button"
+          id="worstOpt3"
+          className="answer-button worst-button"
+          aria-label="Option 3 is worst"
+        >
+          Worst
+        </button>
+      </div>
+
+      <hr />
+
+      <div id="answerOption4" className="answer-option">
+        <button
+          type="button"
+          id="bestOpt4"
+          className="answer-button best-button"
+          aria-label="Option 4 is best"
+        >
+          Best
+        </button>
+        <span id="optText4" className="option-text" aria-label="Option 4">
+          {props.optText4}
+        </span>
+        <button
+          type="button"
+          id="worstOpt4"
+          className="answer-button worst-button"
+          aria-label="Option 4 is worst"
+        >
+          Worst
+        </button>
+      </div>
+      <hr />
+
+      <button type="submit" onClick={props.submitHandling}>
+        Submit
+      </button>
+    </section>
+  );
+};
+
+export default Question;
