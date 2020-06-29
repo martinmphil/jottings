5416e473b4270657b7d1ce3d3d7028518fbd52c4
Martin <martin.mphil@gmail.com>
2020-06-29 09:16:14 +0100

    stand alone app

--- orunder/orunder2021.ts
+++
@@ -1,9 +0,0 @@
-const instruct = (n = "10", us = "10", them = "10") => {
-  return `Po roll ${n}. Also us is ${us} and them is ${them}.`;
-};
-
-// The reset button requires this fn to reset the output html tag.
-const setup = () => {
-  document.querySelector("output").innerText = "Roll 3d20 =orUnder 4";
-};
-setup();
