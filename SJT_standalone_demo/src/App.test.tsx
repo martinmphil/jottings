5c07cccbfc5664d814e26c780ea45b2b6cb66032
Martin <37618836+martinmphil@users.noreply.github.com>
2024-10-03 11:58:48 +0100

    Delete outdated react code in SJT_standalone_demo directory

--- SJT_standalone_demo/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import ReactDOM from "react-dom";
-
-test("App renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
